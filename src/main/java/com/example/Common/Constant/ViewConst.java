f2277ee27e81283223705fb12ab877b51d8b3485
nakanishi563 <166482259+nakanishi563@users.noreply.github.com>
2024-05-16 16:27:18 +0900

    Create ViewConst.java

---
+++ src/main/java/com/example/Common/Constant/ViewConst.java
@@ -0,0 +1,32 @@
+package com.example.common.constant;
+
+/**
+ * View定数クラス
+ * 
+ */
+public class ViewConst {
+
+	/** ホーム画面 */
+	public static final String HOME = "home";
+	
+	/** 商品検索結果画面 */
+	public static final String SORTRESULT = "sortResult";
+	
+	/** カート画面 */
+	public static final String CART = "cart";
+	
+	/** 商品詳細画面 */
+	public static final String ITEMDETAIL = "itemdetail";
+	
+	/** 商品登録完了画面 */
+	public static final String REGISTITEM = "registItem";
+	
+	/** 注文者情報入力画面 */
+	public static final String ORDERINPUT = "orderInput";
+	
+	/** 注文情報確認画面 */
+	public static final String ORDERCHECK = "orderCheck";
+	
+	/** 注文完了画面 */
+	public static final String ORDERCOMPLETE = "orderComplete";
+}
